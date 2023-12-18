9466c23bb288fd02565fe50fe4c701f9b16dd121
gwubc <99283518+gwubc@users.noreply.github.com>
2023-12-18 15:21:57 -0800

    Update OneTimeEvent.py

--- RemoteHotKey/OneTimeEvent.py
+++ RemoteHotKey/OneTimeEvent.py
@@ -1,19 +1,24 @@
 import json
+import typing
 
 
 class OneTimeEvent:
-    _name = None
-    _timeStamp = None
+    _name: str
+    _timeStamp: float
+    _data: typing.Dict
+    _handled = False
 
-    def __init__(self, data=None):
-        if data is not None:
-            if isinstance(data, str):
-                data = json.loads(data)
-            self.setFields(data.get("name", ""), data.get("timeStamp", 0))
+    def __init__(self, saveData: str=None):
+        if saveData is not None:
+            saveData = json.loads(saveData)
+        else:
+            saveData = {}
+        self.setFields(saveData.get("name", ""), saveData.get("timeStamp", 0), saveData.get("data", {}))
 
-    def setFields(self, name: str, timeStamp: float):
+    def setFields(self, name: str, timeStamp: float, data: typing.Dict):
         self._name = name
         self._timeStamp = timeStamp
+        self._data = data
 
     def getName(self) -> str:
         return self._name
@@ -21,11 +26,20 @@ class OneTimeEvent:
     def getTimeStamp(self) -> float:
         return self._timeStamp
 
+    def getData(self) -> typing.Dict:
+        return self._data
+
+    def handle(self):
+        self._handled = True
+
+    def isHandled(self) -> bool:
+        return self._handled
+
     def toJSON(self):
         return json.dumps(self.toJSONDic())
 
     def toJSONDic(self):
-        jsonDic = {"name": self._name, "timeStamp": self._timeStamp}
+        jsonDic = {"name": self._name, "timeStamp": self._timeStamp, "data": self._data.copy()}
         return jsonDic
 
     def __eq__(self, o: object) -> bool:
